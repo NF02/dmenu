56f38e5d9b53f9572c21cabf797d78e1cc344b3b
NFVblog <66777517+NF02@users.noreply.github.com>
2021-03-05 20:03:21 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -4,8 +4,10 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Noto Color Emoji:size=10"
+	"monospace:size=10",
+	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
