859f7a2f8d3b3b29ca7bf0b3dc89007f0875e8cc
NFVblog <66777517+NF02@users.noreply.github.com>
2021-03-05 21:06:10 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -8,6 +8,8 @@ static const char *fonts[] = {
 	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
 
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
@@ -15,6 +17,15 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
+
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
